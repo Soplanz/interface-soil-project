9f5acdd43f7465c443a8a216e30fe95056f5f1ca
zefanyasoplantila <t-zefanya.soplan@traveloka.com>
2024-03-28 16:48:10 +0700

    Implement the UI and the docker and deploy ML model

---
+++ app/main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, Request
+from datetime import datetime
+from app.utils import get_nearest_hour, get_today_info
+
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+import pickle
+import os
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {'request':request})
+
+@app.get("/moisture", response_class=HTMLResponse)
+async def moisture_view(request: Request):
+    return templates.TemplateResponse("index_moisture.html", {'request': request})
+
+@app.get("/suction", response_class=HTMLResponse)
+async def suction_(request: Request):
+    return templates.TemplateResponse("index_suction.html", {'request': request})
+
+@app.post("/predict/moisture/{latitude}/{longitude}")
+async def predict_moisture(latitude: str, longitude: str):
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    target_hour  = get_nearest_hour(current_time)
+    
+    api_dict, feature_cols = get_today_info(latitude=latitude, longitude=longitude)
+    index = api_dict['timestamp'].index(target_hour)
+
+    features_data = {}
+    features_data['longitude'] = longitude
+    features_data['latitude']  = latitude
+
+    features = [longitude, latitude]
+    for col in feature_cols:
+        features_data[col] = api_dict[col][index]
+        features.append(features_data[col])
+
+    with open('app/moisture_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+        features_data['predicted_moisture'] = model.predict([features])[0]
+    
+    return features_data
+
+@app.post("/predict/suction/{latitude}/{longitude}")
+async def predict_suction(latitude: str, longitude: str):
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    target_hour  = get_nearest_hour(current_time)
+    
+    api_dict, feature_cols = get_today_info(latitude=latitude, longitude=longitude)
+    index = api_dict['timestamp'].index(target_hour)
+
+    features_data = {}
+    features_data['longitude'] = longitude
+    features_data['latitude']  = latitude
+
+    features = [longitude, latitude]
+    for col in feature_cols:
+        features_data[col] = api_dict[col][index]
+        features.append(features_data[col])
+
+    with open('app/suction_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+        features_data['predicted_suction'] = model.predict([features])[0]
+    
+    return features_data
